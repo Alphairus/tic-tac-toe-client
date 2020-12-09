cc92908b428012ca6f0041453d34b3a29680c2b0
alpharius <cmcauliffe006@gmail.com>
2020-12-09 01:42:05 -0500

    First Commit TTT
    -worked on workflow/user-stories/plan
    -curl-scripts
    -authentication
    -html/scss

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://tic-tac-toe-api-production.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
