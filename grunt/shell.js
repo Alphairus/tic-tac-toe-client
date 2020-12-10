0b9c48910d40ff958327e9665a64862bb8699fce
alpharius <cmcauliffe006@gmail.com>
2020-12-09 19:17:57 -0500

    Changes 12/9 7:00pm pre-deploy
    committed changes to aliases.json and shell.js

--- grunt/shell.js
+++ grunt/shell.js
@@ -41,7 +41,7 @@ module.exports = {
     command: [
       'git branch -D gh-pages || echo "so not removed"',
       'git checkout --orphan gh-pages',
-      "git rm --cached '*'"
+      'git rm --cached *'
     ].join(' && ')
   },
   'deploy-publish': {
