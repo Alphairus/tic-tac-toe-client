3d3748bc8b5a706f3fecd9960e0385ff7299c707
alpharius <cmcauliffe006@gmail.com>
2020-12-10 19:31:31 -0500

    changed placeholder to value and added pass_confirm

--- curl-scripts/authentication/sign-up.sh
+++ curl-scripts/authentication/sign-up.sh
@@ -1,15 +1,13 @@
-API="https://tic-tac-toe-api-development.herokuapp.com"
-URL_PATH="/sign-up"
-
-curl "${API}${URL_PATH}" \
-  --include \
-  --request POST \
-  --header "Content-Type: application/json" \
-  --data '{
-    "credentials": {
-      "email": "'"${EMAIL}"'",
-      "password": "'"${PASSWORD}"'",
-      "password_confirmation": "'"${PASSWORD}"'"
+js
+curl "https://tic-tac-toe-api-development.herokuapp.com/sign-up" \
+--include \
+--request POST \
+--header "Content-Type: application/json" \
+--data '{
+  "credentials": {
+    "email": "'"${EMAIL}"'",
+    "password": "'"${PASSWORD}"'",
+    "password_confirmation": "'"${PASSWORD}"'"
     }
   }'
 
