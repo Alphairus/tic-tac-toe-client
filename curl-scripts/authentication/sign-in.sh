de0f0e3bc4c781699cb1195d0d3c902204021c48
alpharius <cmcauliffe006@gmail.com>
2020-12-10 19:44:28 -0500

    Update - Sign up/in working from this point
    -12/10 7:45pm
    Sign up 201 creation terminal - response and no 422 error from server.
    Sign in also confirmed and working via grunt serve

--- curl-scripts/authentication/sign-in.sh
+++ curl-scripts/authentication/sign-in.sh
@@ -1,4 +1,4 @@
-curl "https://tic-tac-toe-api-development.herokuapp.com" \
+curl "https://tic-tac-toe-api-development.herokuapp.com/sign-in" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
